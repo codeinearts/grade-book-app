61845953cf9f7a6408bcbb70eb87a89814bbb4a2
Victor Gallardo <vg.temporal.001@gmail.com>
2024-09-28 03:17:37 -0300

    modifica las capas separando por servicio users y tasks

--- controllers/tasks_controller.py
+++ controllers/tasks_controller.py
@@ -1,6 +1,6 @@
 from flask import request, redirect, url_for, Blueprint, render_template
 
-from services.service import Service
+from services.tasks_service import Service
 
 tasks_bp = Blueprint("tasks", __name__)
 service = Service()
@@ -9,16 +9,16 @@ in_construction = 'in-construction.html'
 
 @tasks_bp.route('/tasks', methods=['GET'])
 def read():
-    return render_template(in_construction)
+    return service.read()
 
 @tasks_bp.route('/tasks/create', methods=['GET'])
 def create():
-    return render_template(in_construction)
+    return service.create()
 
 @tasks_bp.route('/tasks/update', methods=['GET'])
 def update():
-    return render_template(in_construction)
+    return service.update()
 
 @tasks_bp.route('/tasks/delete', methods=['GET'])
 def delete():
-    return render_template(in_construction)
+    return service.delete()
--- services/tasks_service.py
+++ services/tasks_service.py
@@ -1,4 +1,4 @@
-from repositories.users_repository import Repository
+from repositories.tasks_repository import Repository
 from flask import render_template
 
 in_construction: str = "in-construction.html"
@@ -12,3 +12,10 @@ class Service:
 
     def read(self) -> str:
         return render_template(in_construction)
+
+    def update(self) -> str:
+        return render_template(in_construction)
+
+    def delete(self) -> str:
+        return render_template(in_construction)
+
