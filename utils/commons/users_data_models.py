3db6dadb95917e6e072c3c87754193983ed3c9ef
Victor Gallardo <vg.temporal.001@gmail.com>
2024-10-11 21:03:00 -0300

    modifica user data model para incrementar la seguridad de validacion de datos de creacion de usuario

--- utils/commons/users_data_models.py
+++ utils/commons/users_data_models.py
@@ -1,14 +1,20 @@
-from pydantic import BaseModel, model_validator, constr, EmailStr
-from werkzeug.security import generate_password_hash, check_password_hash
-from typing import Optional, ClassVar, Dict, Any, Tuple
+from pydantic import (
+    BaseModel,
+    StringConstraints,
+    model_validator, 
+    EmailStr
+
+)
+from werkzeug.security import generate_password_hash
+from typing import Optional, Dict, Any, Annotated
 
 
 class User(BaseModel):
 
     hashed_user_rut: str
     rut: str
-    name: str
-    lastname: str
+    name: Annotated[str, StringConstraints(min_length=1, max_length=50)]
+    lastname: Annotated[str, StringConstraints(min_length=1, max_length=50)]
     email: Optional[EmailStr] = None
 
     @model_validator(mode="before")
@@ -18,14 +24,27 @@ class User(BaseModel):
         Mapea los campos de data antes
         de crear el objeto como objeto de la clase.
         """
+        rut = data.get("rut")
+        if not cls.validate_rut(rut):
+            raise ValueError("RUT inválido")
 
-        hashed_user_rut = generate_password_hash(str(data.get("rut")), method='pbkdf2:sha256')
+        hashed_user_rut = generate_password_hash(str(rut), method='pbkdf2:sha256')
 
-        data = dict(
+        return dict(
             hashed_user_rut=hashed_user_rut,
-            rut=data.get("rut"),
+            rut=rut,
             name=data.get("name"),
             lastname=data.get("lastname"),
             email=data.get("email"),
         )
-        return data
+
+    @staticmethod
+    def validate_rut(rut: str) -> bool:
+        """
+        Aquí puedes agregar las reglas de validación del RUT según el formato esperado.
+        """
+        # Implementa la lógica de validación según el formato de tu país.
+        if len(rut) < 8 or len(rut) > 12:  # Ejemplo de validación de longitud
+            return False
+        # Agregar validaciones adicionales si es necesario (e.g., dígito verificador)
+        return True
